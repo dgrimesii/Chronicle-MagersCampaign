f585088e0b25d776d9419b413bd8619f3447aa53
David Grimes <dgrimesii@yahoo.com>
2026-04-03 10:57:06 -0400

    Ran build.js

--- shared/config.js
+++ shared/config.js
@@ -5,5 +5,5 @@
 window.CHRONICLE_CONFIG = {
   proxyUrl:        'https://script.google.com/macros/s/AKfycby8_YLLYic6TU_8HOBXmGDrGNXhzf1ZIVepdeE6qCJR-P3otd6GB0Ll7gnKZamaLHYR/exec',
   campaignFileId:  '1FPs6fvLpmZaeqAe95gXmvqiCRgMr5aFm',
-  embeddedDataDate: '2026-04-01',
+  embeddedDataDate: '2026-04-03',
 };
